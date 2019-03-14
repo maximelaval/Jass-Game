c56c041fd24d1eebda814050c0f097681e34add9
Maxime Laval <lavalmaxime@hotmail.com>
2019-03-14 21:43:42 +0100

    test

--- src/ch/epfl/javass/Javass.java
+++ src/ch/epfl/javass/Javass.java
@@ -7,6 +7,7 @@ import ch.epfl.javass.jass.Trick;
 public class Javass {
     public static void main(String[] args) {
         int i = 0b10_00_0000_000000_000000_110010_110001;
+        
         long h = 0b0000000_000000000_0000000_110000110_1000100_000001000_0000011_000000001L;
 //        System.out.println(PackedTrick.trump(i));
 //        System.out.println(Integer.toBinaryString(-2113929742));
