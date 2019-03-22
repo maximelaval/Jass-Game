de371250c8a77de9fed17a285ebbecf929375f91
Maxime Laval <lavalmaxime@hotmail.com>
2019-03-22 09:47:06 +0100

    updated JassGame

--- src/ch/epfl/javass/jass/JassGame.java
+++ src/ch/epfl/javass/jass/JassGame.java
@@ -2,16 +2,17 @@ package ch.epfl.javass.jass;
 
 import java.util.*;
 
-import static java.time.chrono.JapaneseEra.values;
-
 public final class JassGame {
 
     private TurnState turnState;
     private Random shuffleRng;
     private Random trumpRng;
-    private  Map<PlayerId, Player>players;
-   private  Map<PlayerId, String> playerNames;
+    private Map<PlayerId, Player> players;
+    private Map<PlayerId, String> playerNames;
+
+
     public JassGame(long rngSeed, Map<PlayerId, Player> players, Map<PlayerId, String> playerNames) {
+
         this.players = java.util.Collections.unmodifiableMap(new EnumMap<>(players));
         this.playerNames = java.util.Collections.unmodifiableMap(new EnumMap<>(playerNames));
         Random rng = new Random(rngSeed);
@@ -25,11 +26,40 @@ public final class JassGame {
          }
 
     
-    
-    
     public void advanceToEndOfNextTrick() {
         if (!isGameOver()) {
-
+            if (turnState.trick().index() == 0) {
+                shuffleAndDistribute();
+                startTurn();
+            } else {
+                nextPlayerToPlay();
+            }
         }
+    }
+
+    private void shuffleAndDistribute() {
+        List<Card> deck = constructCardList();
+        Collections.shuffle(deck, shuffleRng);
+        for (int i = 0; i < 4; ++i){
+            players.get(i).updateHand(CardSet.of(deck.subList(i * 9, i * 9 + 8)));
+        }
+    }
+
+    private void startTurn() {
+
+    }
+
+    private void nextPlayerToPlay() {
+
+    }
+
+    private List<Card> constructCardList() {
+        List<Card> list = Collections.emptyList();
+        for (int i = 0; i < Card.Color.COUNT; i++) {
+            for (int j = 0; j < Card.Rank.COUNT; ++i) {
+                list.add(Card.of(Card.Color.values()[i], Card.Rank.values()[j]));
+            }
+        }
+        return list;
     }
 }
