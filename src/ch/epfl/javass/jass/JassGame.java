8a46c4df32453c5e9f6bba0c120d53129e282e73
maximelaval <48682328+maximelaval@users.noreply.github.com>
2019-03-22 05:16:02 +0100

    Update JassGame.java

--- src/ch/epfl/javass/jass/JassGame.java
+++ src/ch/epfl/javass/jass/JassGame.java
@@ -7,19 +7,26 @@ import static java.time.chrono.JapaneseEra.values;
 public final class JassGame {
 
     private TurnState turnState;
-
+    private Random shuffleRng;
+    private Random trumpRng;
+    private  Map<PlayerId, Player>players;
+   private  Map<PlayerId, String> playerNames;
     public JassGame(long rngSeed, Map<PlayerId, Player> players, Map<PlayerId, String> playerNames) {
-
+        this.players = java.util.Collections.unmodifiableMap(new EnumMap<>(players));
+        this.playerNames = java.util.Collections.unmodifiableMap(new EnumMap<>(playerNames));
         Random rng = new Random(rngSeed);
         this.shuffleRng = new Random(rng.nextLong());
         this.trumpRng = new Random(rng.nextLong());
-        // … reste du constructeur
+     
     }
 
     public boolean isGameOver() {
+       return (turnState.packedScore()>=Jass.WINNING_POINTS);
+         }
 
-    }
-
+    
+    
+    
     public void advanceToEndOfNextTrick() {
         if (!isGameOver()) {
 
