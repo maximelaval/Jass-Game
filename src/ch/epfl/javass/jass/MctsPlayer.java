f1adb7c769aeee34783e775578e9129d62465a98
Maxime Laval <lavalmaxime@hotmail.com>
2019-03-26 15:35:37 +0100

    updated JassGame

--- src/ch/epfl/javass/jass/MctsPlayer.java
+++ src/ch/epfl/javass/jass/MctsPlayer.java
@@ -1,37 +1,44 @@
-//package ch.epfl.javass.jass;
-//
-//import java.util.Map;
-//import java.util.Random;
-//
-//import static ch.epfl.javass.jass.Jass.HAND_SIZE;
-//
-//public final class MctsPlayer implements Player  {
-//
-//    private static class Node{
-//
-//        private TurnState turnState;
-//        private Node[] children;
-//        private CardSet upcomingChildren;
-//        private int points;
-//        private int finishedRandomTurns;
-//    }
-//
-//    private final Random rng;
-//
-//    MctsPlayer(PlayerId ownId, long rngSeed, int iterations) {
-//        if (iterations < HAND_SIZE)
-//            throw new IllegalArgumentException();
-//        else {
-//
-//        }
-//    }
-//
-//    @Override
-//    public Card cardToPlay(TurnState state, CardSet hand) {
-//        // creer pleins de methodes privées
-//
-//        return null;
-//    }
-//
-//
-//}
+package ch.epfl.javass.jass;
+
+
+import java.util.SplittableRandom;
+
+import static ch.epfl.javass.jass.Jass.HAND_SIZE;
+
+public final class MctsPlayer implements Player  {
+
+    private static class Node{
+
+        private TurnState turnState;
+        private Node[] children;
+        private CardSet upcomingChildren;
+        private int points;
+        private int finishedRandomTurns;
+
+        private Node() {
+
+        }
+
+    }
+
+    private final SplittableRandom mctsRng;
+
+    MctsPlayer(PlayerId ownId, long rngSeed, int iterations) {
+        if (iterations < HAND_SIZE)
+            throw new IllegalArgumentException();
+        else {
+            SplittableRandom rng = new SplittableRandom(rngSeed);
+            this.mctsRng = new SplittableRandom(rng.nextLong());
+
+        }
+    }
+
+    @Override
+    public Card cardToPlay(TurnState state, CardSet hand) {
+        // creer pleins de methodes privées
+
+        return null;
+    }
+
+
+}
